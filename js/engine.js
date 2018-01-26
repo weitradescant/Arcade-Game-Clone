aba03ea938cb3ef0a01f548ec78d1ae4de36920d
WeiTradescant <weitradescant@163.com>
2018-01-26 18:33:57 +0800

    实现了人物、虫子可移动

--- js/engine.js
+++ js/engine.js
@@ -140,6 +140,10 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png',
         'images/char-boy.png'
     ]);
     Resources.onReady(init);
